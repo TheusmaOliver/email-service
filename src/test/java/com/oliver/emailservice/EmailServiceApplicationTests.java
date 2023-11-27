2f031de4adb8bff8222075be4713e65506c2b6a4
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-27 15:03:45 -0300

    initial commit

---
+++ src/test/java/com/oliver/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliver.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
